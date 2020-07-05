bb29127c3f103ab3a760c02d03cd2420d05f4cc4
Emanuel Moraes <emanuelmoraes297@gmail.com>
2020-07-05 08:41:27 -0300

    env.sh não sobrescreve valores customizáveis

--- src/env.sh
+++ src/env.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Localização da pasta que conterá as informações registradas na aplicação
-DATA_FOLDER=$HOME/.lmg
+[ -z "$DATA_FOLDER" ] && DATA_FOLDER=$HOME/.lmg
 
 # Nome da pasta que conterá a lista de tarefas
-TASK_FOLDER=task
+[ -z "$TASK_FOLDER" ] && TASK_FOLDER=task
 
 # Nome da pasta que conterá a lista de tarefas que foram executadas
-TASK_RUNNER_FOLDER=task-runner
+[ -z "$TASK_RUNNER_FOLDER" ] && TASK_RUNNER_FOLDER=task-runner
 
 # Nome da pasta que conterá a lista de informações sobre o uso do sistema
-INFO_FOLDER=info
+[ -z "$INFO_FOLDER" ] && INFO_FOLDER=info
 
 # Nome do script executável para executar a tarefa do tipo "script"
-TASK_SCRIPT_NAME_UP="task-up.sh"
+[ -z "$TASK_SCRIPT_NAME_UP" ] && TASK_SCRIPT_NAME_UP="task-up.sh"
 
 # Nome do script executável para executar a tarefa do tipo "script"
-TASK_SCRIPT_NAME_DOWN="task-down.sh"
+[ -z "$TASK_SCRIPT_NAME_DOWN" ] && TASK_SCRIPT_NAME_DOWN="task-down.sh"
 
 # Nome do arquivo que conterá o nome do tipo da tarefa
-TASK_TYPE_FILENAME="task-type"
+[ -z "$TASK_TYPE_FILENAME" ] && TASK_TYPE_FILENAME="task-type"
 
 # Nome do arquivo que conterá o nome da tarefa
-TASK_NAME_FILENAME="task-name"
+[ -z "$TASK_NAME_FILENAME" ] && TASK_NAME_FILENAME="task-name"
 
 # Nome do arquivo que conterá a mensagem da tarefa
-TASK_MESSAGE_FILENAME="task-message"
+[ -z "$TASK_MESSAGE_FILENAME" ] && TASK_MESSAGE_FILENAME="task-message"
 
 # CÓDIGOS DE ERRO DO SCRIPT (90-119)
 ERR_UNEXPECTED=90
@@ -42,4 +42,4 @@ RED="\e[31;1m"
 END_COLOR="\e[m"
 
 # TEMAS
-ERROR_THEME="$RED"
+[ -z "$ERROR_THEME" ] && ERROR_THEME="$RED"
