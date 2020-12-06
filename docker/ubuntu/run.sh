f75874effd1338045b397c2bab7cc70814bd097b
emanuelmoraes-dev <emanuelmoraes297@gmail.com>
2020-12-06 16:26:37 -0300

    version 0.0.13: data/ e docker/ atualizados

--- docker/ubuntu/run.sh
+++ docker/ubuntu/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 [ -z "$NAME" ]    && NAME="lmg-ubuntu"
-[ -z "$VERSION" ] && VERSION=0.0.12
+[ -z "$VERSION" ] && VERSION=0.0.13
 [ -z "$DIRNAME" ] && DIRNAME="$(dirname "$0")/../.."
 [ -z "$USER" ]    && USER="root"
 
 DIRNAME="$(sh -c "cd \"$DIRNAME\" && pwd")"
 
-docker run -ti --rm -v "$DIRNAME/src:/mnt/app" -v "$DIRNAME/data:/mnt/data" -e LMG_DATA_FOLDER="$DIRNAME/data" --user="$USER" "$NAME:$VERSION" "$@"
+docker run -ti --rm -v "$DIRNAME/src:/mnt/app" -v "$DIRNAME/data:/mnt/data" -e LMG_DATA_FOLDER="/mnt/data" --user="$USER" "$NAME:$VERSION" "$@"
